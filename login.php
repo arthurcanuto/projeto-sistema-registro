09c2ddb787f510416eb0beb3b7bec5593fb8bbde
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-17 15:42:35 -0300

    Update login.php

--- login.php
+++ login.php
@@ -15,17 +15,23 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Conexão realizada com sucesso";
-} catch(PDOException $e) {
-  echo "Conexão falhou: " . $e->getMessage();
-}
+}  catch(PDOException $e) {
+    echo "Conexão falhou: " . $e->getMessage();
+  }
 // Conexao realizada com sucesso
-$stmt = $conn->prepare('SELECT codigo FROM usuario where email=:email AND senha=md5(:senha');
+$stmt = $conn->prepare('SELECT IdUsuario FROM usuario where email=:email AND senha=md5(:senha)');
 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 $stmt->bindParam(':senha', $senha, PDO::PARAM_STR);
   $stmt->execute();
   // set the resulting array to associative
   $result = $stmt->fetchAll();
-  print_r($result);
+  $qnt_usuarios = count($result);
+    if($qnt_usuarios == 1){
+        echo "Usuário Encontrado!";
+    } else if($qnt_usuarios == 0){
+        echo "Usuário NÃO Encontrado!";
+    }
+   
   $conn = null;
 // 3. Verificar se email e senha estão no BD
 
