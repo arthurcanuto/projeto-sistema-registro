a2745700b40c5cd809e733c816584ac1481533f5
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-20 17:04:54 -0300

    Update cadastra_produto.php

--- cadastra_produto.php
+++ cadastra_produto.php
@@ -1,25 +1,19 @@
 <?php
+
 if(count($_POST)> 0){
     $nome= $_POST["nome_produto"];
     $categoria = $_POST["categoria_produto"];
-    $valor = $_POST["valor_produto"];
+    $valor = $_POST["preco_produto"];
     $foto = $_POST["foto_produto"];
     $info = $_POST["info_produto"];
-
+    
     try{        
         require("connection.php");
         
     $sql = "INSERT INTO produto (nome, categoria, valor, foto, info_adicional, codigo_usuario) VALUES (?,?,?,?,?,?)";
     $stmt= $conn->prepare($sql);
     // To do pegar o codigo do usuario logado
-    $stmt->execute([$nome, $categoria, $valor, $foto, $info, null]);
-    
-    // pegar os produtos armazenados no BD
-    $consulta = $conn->prepare("SELECT * FROM produto");
-    $consulta->execute();
-    // set the resulting array to associative
-    $resultado["produtos"] = $consulta->fetchAll();
-    print_r($resultado["produtos"]); exit;
+    $stmt->execute([$nome, $categoria, $valor, $foto, $info, null]); 
 
 // TODO substituir pelo redirecionamento
     $resultado["msg"] = "Item inserido com sucesso!";
@@ -32,8 +26,19 @@ if(count($_POST)> 0){
     $resultado["msg"] = "Inserção no banco de dados falhou" . $e->getMessage();
     $resultado["cod"] = 0;
     $resultado["style"] = "alert-success";
-    }       
-}
+    } 
+try{
+    // pegar os produtos armazenados no BD
+    $consulta = $conn->prepare("SELECT * FROM produto");
+    $consulta->execute();
+    // set the resulting array to associative
+    $produtos = $consulta->fetchAll(); 
+}catch(PDOException $e) {        
+    $resultado["msg"] = "Erro ao inserir produto no banco de dados falhou" . $e->getMessage();
+    $resultado["cod"] = 0;
+    $resultado["style"] = "alert-danger";
+    }   
 $conn = null;
+}
 include("produto.php");
 ?>
