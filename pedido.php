34a3ab7c8c8fdbf3afdc10652b812ace34c6c45d
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-21 14:15:38 -0300

    Update pedido.php

--- pedido.php
+++ pedido.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
+<?php require_once("connection.php"); ?>
+<?php if(isset( $_SESSION['nome_usuario']) ): ?>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content=""width=device-width, initial-scale="1.0">
+    <title>Pedido</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
         <!-- tela para digitar o pedido -->
         <form action="cadastrar_pedido.php" method="POST">
+            <br>
+            <h5>Olá, <?= $_SESSION["nome_usuario"]; ?>
             <br>
             <h2>Escolha de Itens do Pedido</h2>
             <br>
@@ -26,7 +31,7 @@
         </div>
         <div class="form-group">
         <label for="preco_produto">Preço do Produto:</label>
-        <input type="text" class="form-control" id="preco_produto" name="preco_produto" placeholder="">
+        <input type="number" step=".01" class="form-control" id="preco_produto" name="preco_produto" placeholder="">
         </div>
         <button type="submit" class="btn btn-primary">Adicionar item</button>
         <?php if(isset($resultado)): ?>
@@ -41,3 +46,8 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </html>
+<?php else: ?>
+    <div class="alert alert-danger <?=$resultado["style"]?> ">
+        Você não está logado no sistema.
+    </div>
+<?php endif; ?>
