687b1c0fe3675748c7eabff132ce26bcea8f5ac2
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-21 14:00:55 -0300

    Update cadastrar_pedido.php

--- cadastrar_pedido.php
+++ cadastrar_pedido.php
@@ -1,37 +1,34 @@
 <?php
 // pegar valores do formulario
+require_once("connection.php");
 
-if(count($_POST)> 0){
-    $nome= $_POST["nome_produto"];
-    $qtd = $_POST["qnt_produto"];
-    $obs = $_POST["obs_produto"];
-    $preco = $_POST["preco_produto"];
-}
 
-$servername = "localhost";
-$username = "root";
-$password = "Vacapreta@123";
+if(count($_POST) > 0){
+  $nome= $_POST["nome_produto"];
+  $qtd = $_POST["qnt_produto"];
+  $obs = $_POST["obs_produto"];
+  $preco = $_POST["preco_produto"];
+  $codigo = $_SESSION["codigo_usuario"];
 
-    try{
-        
-    require_once("connection.php");
-    
+  try{     
+      
     $sql = "INSERT INTO item_pedido (IdUsuario, nome_produto, observacao, preco_und, quantidade) VALUES (?,?,?,?,?)";
     $stmt= $conn->prepare($sql);
     // To do pegar o codigo do usuario logado
-    $stmt->execute([null, $nome, $obs, $preco, $qtd]);
+    $stmt->execute([$codigo, $nome, $obs, $preco, $qtd]);
     
     // TODO substituir pelo redirecionamento
-      $resultado["msg"] = "Item inserido com sucesso!";
-      $resultado["cod"] = 1;
-      $resultado["style"] = "alert-success";
+    $resultado["msg"] = "Item inserido com sucesso!";
+    $resultado["cod"] = 1;
+    $resultado["style"] = "alert-success";
+    
+  }catch(PDOException $e) {
       
-    }catch(PDOException $e) {
-        
-    $resultado["msg"] = "Inserção no banco de dados falhou" . $e->getMessage();
-    $resultado["cod"] = 0;
-    $resultado["style"] = "alert-danger";
-    }
+  $resultado["msg"] = "Inserção no banco de dados falhou" . $e->getMessage();
+  $resultado["cod"] = 0;
+  $resultado["style"] = "alert-danger";
+  }
+}  
       
   $conn = null;
   include("pedido.php");
