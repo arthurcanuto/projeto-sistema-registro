bb64accb4bf3e2c21475ac7b83b434287ac2e397
Arthur Canuto <99217088+arthurcanuto@users.noreply.github.com>
2022-10-21 14:16:03 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+
 require_once("connection.php");
 
 if(!empty($_POST['email']) and !empty($_POST['senha'])){
@@ -8,17 +9,28 @@ if(!empty($_POST['email']) and !empty($_POST['senha'])){
   $senha = $_POST['senha'];
 
   // Conexao realizada com sucesso
-  $consulta = $conn->prepare('SELECT IdUsuario FROM usuario where email=:email AND senha=md5(:senha)');
+  $consulta = $conn->prepare("SELECT * FROM usuario where situacao='Habilitado' AND email=:email AND senha=md5(:senha)");
   $consulta->bindParam(':email', $email, PDO::PARAM_STR);
   $consulta->bindParam(':senha', $senha, PDO::PARAM_STR);
   $consulta->execute();
   // set the resulting array to associative
-  $result = $consulta->fetchAll();
-  $qnt_usuarios = count($result);
+  $r = $consulta->fetchAll();
+  // echo "<prev>";
+  // print_r($r);
+  // echo "</prev>";
+  // exit;
+  $qnt_usuarios = count($r);
   if($qnt_usuarios == 1){
     // TODO substituir pelo redirecionamento
-      $resultado["msg"] = "Usuário encontrado!";
-      $resultado["cod"] = "Usuário não encontrado!";
+
+   $_SESSION["email_usuario"] = $email;
+   $_SESSION["nome_usuario"] = $r[0]['Nome'];
+   $_SESSION["codigo_usuario"] = $r[0]['IdUsuario'];
+   header('location:pedido.php');
+  // echo "<prev>";
+  // print_r($_SESSION);
+  // echo "</prev>";
+  // exit;
   } else if($qnt_usuarios == 0){
       $resultado["msg"] = "E-mail ou senha não conferem...";
       $resultado["cod"] = 0;
